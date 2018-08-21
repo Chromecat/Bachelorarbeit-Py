f7b9000c07bd29af3eb36b273db2cdd1b5d8317d
Stefan Dewenter <Stefan@Stefans-MacBook-Pro.local>
2018-08-21 17:50:24 +0200

    array to ndarray

--- main.py
+++ main.py
@@ -5,13 +5,17 @@ import numpy as np
 
 inputaudio = read("audio.wav")
 
-audio = np.array(a[1], dtype=float)
+audio = np.array(inputaudio[1], dtype=float) # a in inputaudio wechseln
 
 f, t, Sxx = signal.spectrogram(audio)
 
-matrix = np.zeros(t, f, float)  # vgl Sxx[f,t]
+#matrix = np.zeros(t, f)  # vgl Sxx[f,t]
+#matrix = np.ndarray((129, 122), np.zeros())
+matrix = np.ndarray(shape=(t.size, f.size),  dtype=float, order='F')
 
-maxwert = np.zeros(t) # für jeden t wert wird ein maxwert gespeichert
+
+
+"""maxwert = np.zeros(t)  # fuer jeden t wert wird ein maxwert gespeichert
 
 for n1 in range(0, t):  #schleife um maxwert zu bestimmen
 
@@ -24,7 +28,7 @@ for n1 in range(0, t):  #schleife um maxwert zu bestimmen
     print (a)
 
 
-for n2 in range(0, t):  #übertrag matrix mit division des maxwert (wertebereich 0-1)
+for n2 in range(0, t):  #uebertrag matrix mit division des maxwert (wertebereich 0-1)
     for m2 in range(0, f):
         x1 = matrix[n2, m2]
         x2 = maxwert[m2]
@@ -41,3 +45,4 @@ for n3 in range(0, t):  # bestimmung des S lautes
 
 for n3 in range(0, t):  # bestimmung des D lautes
     for m3 in range(0, f):
+"""
