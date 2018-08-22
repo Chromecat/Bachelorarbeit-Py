9a0bf75c20b50cebeae1ab154bf79c6957535798
Chromecat <18415198+Chromecat@users.noreply.github.com>
2018-08-22 12:48:44 +0200

    update ohne git desktop
    n in t und m in f

--- main.py
+++ main.py
@@ -22,33 +22,36 @@ maxwert = np.ndarray(shape=(t.size,), dtype=float)
 print(maxwert.shape)
 print(maxwert.dtype)
 
-for n0 in range(0, t.size):  # uebertrag matrix mit division des maxwert (wertebereich 0-1)
-    maxwert[n0] = 0.000001
-    for m0 in range(0, f.size):
-        matrix[n0, m0] = 0.000001
+for t0 in range(0, t.size):  # uebertrag matrix mit division des maxwert (wertebereich 0-1)
+    maxwert[t0] = 0.000001
+    for f0 in range(0, f.size):
+        matrix[t0, f0] = 0.000001
 
-for n1 in range(0, t.size):  # schleife um maxwert zu bestimmen
+for t1 in range(0, t.size):  # schleife um maxwert zu bestimmen
 
     a = 0
 
-    for m1 in range(0, f.size):
-        if Sxx[m1, n1] > a:
-            a = Sxx[m1, n1]
+    for f1 in range(0, f.size):
+        if Sxx[f1, t1] > a:
+            a = Sxx[f1, t1]
 
 #    print (a)
 #    print (n1)
 
 
-for n2 in range(0, t.size):  # uebertrag matrix mit division des maxwert (wertebereich 0-1)
-    for m2 in range(0, f.size):
-        x1 = Sxx[m2, n2]
-        x2 = maxwert[m2]
-        matrix[n2, m2] = x1/x2
+for t2 in range(0, t.size):  # uebertrag matrix mit division des maxwert (wertebereich 0-1)
+    for f2 in range(0, f.size):
+        x1 = Sxx[f2, t2]
+        x2 = maxwert[f2]
+        matrix[t2, f2] = x1/x2
 
-        print(matrix[m2, n2].round(3))
+        print(matrix[f2, t2].round(3))
 
 
 """
+n = t 
+m = f 
+
 for n3 in range(0, t.size): # bestimmung des H lautes
     for m3 in range(0, f.size):  # analog array bauen zum vergleich
         print("n3")
