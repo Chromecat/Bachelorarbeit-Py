4cf355d23d386f94774e9687882b4e1c410fab24
Stefan Dewenter <Stefan@Stefans-MacBook-Pro.local>
2018-09-05 18:06:15 +0200

    update

--- main.py
+++ main.py
@@ -5,6 +5,47 @@ import numpy as np
 import xlsxwriter
 from scipy.signal import get_window
 
+
+def h():
+
+    # bereich bestimmung der laute a3 fuer t3
+    a3size = 100
+    b3size = 100
+    c3size = 100
+
+    a3true = 0
+    b3true = 0
+    c3true = 0
+
+    a3 = np.ndarray(shape=(a3size + 2,), dtype=int)   # array erstellung
+    b3 = np.ndarray(shape=(b3size + 2,), dtype=int)   # array erstellung
+    c3 = np.ndarray(shape=(c3size + 2,), dtype=int)   # array erstellung
+
+    x3 = 0
+    for r3 in range(0, a3size + 1):    # hier berreich des filters bauen
+
+        a3[r3+1] = x3 + 100      # filter von 0 + 100 bis 200 (a3size = 100)
+        x3 = x3 + 1
+
+    #    print (a3)
+
+    a3[0] = 324  # summe setzen
+
+    for t3 in range(0, t.size):  # bestimmung des H lautes itterativ ueber die zeit
+        s3 = 0
+        for aa3 in range(1, a3.size):   # itterativ ueber alle elemente des array ausser 0
+            s3 += matrix[t3, a3[aa3]]  # aufsummieren der jeweiligen elemente in der matrix
+
+        if s3 >= a3[0]:
+            a3true = 1
+            break
+
+    if a3true > 0 and b3true > 0 and c3true > 0:
+        twert[0] = t3
+    print ("works")
+    return
+
+
 inputaudio = read("audio1.wav")  # lesen des WAV-files
 
 audio = np.array(inputaudio[1], dtype=float)  # inputaudio in ndarray audio transformieren
@@ -59,34 +100,11 @@ for t3 in range(0, t.size):
 workbook.close()
 """
 
+h()
 
-# bereich bestimmung der laute a3 fuer t3
-a3size = 100
+"""
 
-a3 = np.ndarray(shape=(a3size + 2,), dtype=int)   # array erstellung
-
-x3 = 0
-for r3 in range(0, a3size + 1):    # hier berreich des filters bauen
-
-    a3[r3+1] = x3 + 100      # filter von 0 + 100 bis 200 (a3size = 100)
-    x3 = x3 + 1
-
-#    print (a3)
-
-a3[0] = 324  # summe setzen
-
-for t3 in range(0, t.size):  # bestimmung des H lautes itterativ ueber die zeit
-    s3 = 0
-    for aa3 in range(1, a3.size):   # itterativ ueber alle elemente des array ausser 0
-        s3 += matrix[t3, a3[aa3]]  # aufsummieren der jeweiligen elemente in der matrix
-
-    if s3 >= a3[0]:
-        twert[0] = t3
-        print (t3)
-        break
-
-
-newaudio = AudioSegment.from_wav("audio.wav")
+newaudio = AudioSegment.from_wav("audio1.wav")
 
 silence = AudioSegment.silent(duration=250)
 
@@ -102,3 +120,5 @@ thirddslice = t1[-(twert[5]-twert[4]):]
 finalaudio = silence + firstslice + silence + secondslice + silence + thirddslice + silence
 
 finalaudio.export("Ausgabe.wav", format="wav")
+
+"""
