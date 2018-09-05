9c9a341643ebf61ca27111ba1954d4a3ce53158c
Stefan Dewenter <Stefan@Stefans-MacBook-Pro.local>
2018-09-05 17:27:17 +0200

    filter 0,2 bugfix

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ audio = np.array(inputaudio[1], dtype=float)  # inputaudio in ndarray audio tran
 f, t, Sxx = signal.spectrogram(audio, window=get_window('hamming', 1000), noverlap=500)  # spektrogramm bilden
 
 #print(f*100000)
-print(t/100000)
-print(t.shape)
+print(t/100000)  # in sec
+print(t.shape)  # size
 
 #print(Sxx.shape)
 #print(Sxx.dtype)
@@ -63,81 +63,27 @@ workbook.close()
 # bereich bestimmung der laute a3 fuer t3
 a3size = 100
 
-a3 = np.ndarray(shape=(a3size + 2,), dtype=int)   # array erstellung muss die groesse besitze
+a3 = np.ndarray(shape=(a3size + 2,), dtype=int)   # array erstellung
 
 x3 = 0
 for r3 in range(0, a3size + 1):    # hier berreich des filters bauen
 
-    a3[r3+1] = x3 + 100
+    a3[r3+1] = x3 + 100      # filter von 0 + 100 bis 200 (a3size = 100)
     x3 = x3 + 1
 
-    print (a3)
+#    print (a3)
 
-a3[0] = 324
+a3[0] = 324  # summe setzen
 
-for t3 in range(0, t.size):  # bestimmung des H lautes
+for t3 in range(0, t.size):  # bestimmung des H lautes itterativ ueber die zeit
     s3 = 0
-    for aa3 in range(0, a3.size):
-        s3 = s3 + matrix[t3, a3[aa3]]
-    
+    for aa3 in range(1, a3.size):   # itterativ ueber alle elemente des array ausser 0
+        s3 += matrix[t3, a3[aa3]]  # aufsummieren der jeweiligen elemente in der matrix
+
     if s3 >= a3[0]:
         twert[0] = t3
+        print (t3)
         break
-
-if twert[0] > 0:
-    print ("twert")
-    print (twert[0])
-
-
-"""
-
-
-#    for f3 in range(0, f.size):
-#        print("t3")
-        
-a4 = np.ndarray(shape=(x), dtype=int)   # array erstellung 
-a4[0] = 2342                            # erster wert = summe
-a4[1] = 1                               # welche arrayelemente summiert werden sollen
-a4[2] = 2
-a4[3] = 3
-a4[4] = 4
-a4[5] = 5
-a4[6] = 6
-a4[7] = 7
-a4[8] = 8
-a4[9] = 9
-
-for t4 in range(0, t.size):  # bestimmung des S lautes
-
-
-
-
-
-#    for f4 in range(0, f.size):
-#        print("t4")
-        
-a5 = np.ndarray(shape=(x), dtype=int)   # array erstellung 
-a5[0] = 2342                            # erster wert = summe
-a5[1] = 1                               # welche arrayelemente summiert werden sollen
-a5[2] = 2
-a5[3] = 3
-a5[4] = 4
-a5[5] = 5
-a5[6] = 6
-a5[7] = 7
-a5[8] = 8
-a5[9] = 9
-
-for t5 in range(0, t.size):  # bestimmung des D lautes
-
-
-
-
-
-#    for f5 in range(0, f.size):
-#        print("t5")
-        
-
 
 
 newaudio = AudioSegment.from_wav("audio.wav")
@@ -156,5 +102,3 @@ thirddslice = t1[-(twert[5]-twert[4]):]
 finalaudio = silence + firstslice + silence + secondslice + silence + thirddslice + silence
 
 finalaudio.export("Ausgabe.wav", format="wav")
-
-"""
