09af2e18c2617a84b05e10de8787d14cecec796a
Stefan Dewenter <Stefan@Stefans-MacBook-Pro.local>
2018-09-14 11:44:28 +0200

    clearing

--- main.py
+++ main.py
@@ -1,52 +1,62 @@
-from pydub import AudioSegment
-from scipy.io.wavfile import read
-from scipy import signal
-import numpy as np
-import xlsxwriter
-from scipy.signal import get_window
+from pydub import AudioSegment  #pydub for audio processing
+from scipy.io.wavfile import read  #wavfile.read for import wav-file
+from scipy import signal  # scipy for signalprocessing
+import numpy as np  # numpy as options for complex arrays
+import xlsxwriter  # exporting excel files
+from scipy.signal import get_window  # differen windows for fourier transformation
 
 
-def trigger():
+def trigger1():  # def a filter detecting an [ha] sound
 
-    # bereich bestimmung der laute a3 fuer t3
     a3size = 100
     b3size = 100
-    c3size = 100
 
     a3true = 0
     b3true = 0
-    c3true = 0
 
     a3 = np.ndarray(shape=(a3size + 2,), dtype=int)   # array erstellung
     b3 = np.ndarray(shape=(b3size + 2,), dtype=int)   # array erstellung
-    c3 = np.ndarray(shape=(c3size + 2,), dtype=int)   # array erstellung
 
     x3 = 0
-    for r3 in range(0, a3size + 1):    # hier berreich des filters bauen
+    for r3 in range(0, a3size + 1):
 
-        a3[r3+1] = x3 + 100      # filter von 0 + 100 bis 200 (a3size = 100)
+        a3[r3+1] = x3 + 100      # filter from 0 + 100 to 200 (a3size = 100)
         x3 = x3 + 1
 
-    #    print (a3)
+    a3[0] = 324  # Value to exceed
 
-    a3[0] = 324  # summe setzen
-
-    for t3 in range(0, t.size):  # bestimmung des H lautes itterativ ueber die zeit
+    for t3 in range(0, t.size):  # iterrativ detection of sound
         s3 = 0
         for aa3 in range(1, a3.size):   # itterativ ueber alle elemente des array ausser 0
-            s3 += matrix[t3, a3[aa3]]  # aufsummieren der jeweiligen elemente in der matrix
+            s3 += matrix[t3, a3[aa3]]  # summation of values
 
-        if s3 >= a3[0]:
+        if s3 >= a3[0]:  # case of detection
             a3true = 1
+            x3 = 0
+            for r3 in range(0, a3size + 1):
+                a3[r3 + 1] = x3 + 100  # filter from 0 + 100 to 200 (a3size = 100)
+                x3 = x3 + 1
+
+            a3[0] = 324  # Value to exceed
+
+            for t3 in range(0, t.size):  # iterrativ detection of sound
+                s3 = 0
+                for aa3 in range(1, a3.size):  # itterativ ueber alle elemente des array ausser 0
+                    s3 += matrix[t3, a3[aa3]]  # summation of values
+
+                if s3 >= a3[0]:  # case of detection
+                    a3true = 1
+                    break
             break
 
-    if a3true > 0 and b3true > 0 and c3true > 0:
+
+
+    if a3true > 0 and b3true > 0:
         twert[0] = t3
-    print ("works")
     return
 
 
-inputaudio = read("audio1.wav")  # lesen des WAV-files
+inputaudio = read("audio.wav")  # lesen des WAV-files
 
 audio = np.array(inputaudio[1], dtype=float)  # inputaudio in ndarray audio transformieren
 
@@ -92,15 +102,16 @@ for t2 in range(0, t.size):  # itteration mit uebertrag und normierung auf werte
             matrix[t2, f2] = x1 / x2
 
 
-workbook = xlsxwriter.Workbook('audio4data.xlsx')  # in excel schreiben
+workbook = xlsxwriter.Workbook('audiodata.xlsx')  # export excel file
 worksheet = workbook.add_worksheet()
-for t3 in range(0, t.size):
-    for f3 in range(0, f.size):
-        worksheet.write((f.size - f3), t3, matrix[t3, f3])
+for t4 in range(0, t.size):
+    for f4 in range(0, f.size):
+        worksheet.write((f.size - f4), t4, matrix[t4, f4])
 workbook.close()
 
-trigger()
+trigger1()  # calling function to detect sound [ha]
 
+# setting t values to 0
 twert[0] = 0
 twert[1] = 0
 twert[2] = 0
@@ -108,21 +119,20 @@ twert[3] = 0
 twert[4] = 0
 twert[5] = 0
 
+newaudio = AudioSegment.from_wav("audio.wav")  # reread audiofile
 
+silence = AudioSegment.silent(duration=250)  # silence between audiosamples/slices
 
-newaudio = AudioSegment.from_wav("audio1.wav")
+# cutting audio into slices
+tt1 = newaudio[:twert[1]]
+firstslice = tt1[-(twert[1]-twert[0]):]
 
-silence = AudioSegment.silent(duration=250)
+tt2 = newaudio[:twert[3]]
+secondslice = tt2[-(twert[3]-twert[2]):]
 
-t1 = newaudio[:twert[1]]
-firstslice = t1[-(twert[1]-twert[0]):]
+tt3 = newaudio[:twert[5]]
+thirddslice = tt3[-(twert[5]-twert[4]):]
 
-t2 = newaudio[:twert[3]]
-secondslice = t1[-(twert[3]-twert[2]):]
+finalaudio = silence + firstslice + silence + secondslice + silence + thirddslice + silence  # arange slices
 
-t3 = newaudio[:twert[5]]
-thirddslice = t1[-(twert[5]-twert[4]):]
-
-finalaudio = silence + firstslice + silence + secondslice + silence + thirddslice + silence
-
-finalaudio.export("Ausgabe.wav", format="wav")
+finalaudio.export("Ausgabe.wav", format="wav")  # export wav file
